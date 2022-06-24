919a19ef9d0490907b09dc1c4d04f12419a5341a
SettingDust <settingdust@gmail.com>
2022-06-24 15:53:12 +0800

    feat: allow async download

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,25 @@ async function main(): Promise<void> {
   try {
     const fileURL = core.getInput('file-url', {required: true}).split('\n')
     const fileLocation: string = core.getInput('location') || process.cwd()
-    const filePaths: string[] = []
-    await Promise.all(
-      fileURL.map(async url => {
-        core.info('Downloading file:')
-        core.info(`\turl: ${url}`)
-        core.info(`\tlocation: ${path.resolve(fileLocation)}`)
-        const download = new DownloaderHelper(url, fileLocation)
-        await download.start()
-        download.on('end', ({filePath}) => filePaths.push(filePath))
-      })
+    core.setOutput(
+      'files',
+      await Promise.all(
+        fileURL.map(
+          async url =>
+            new Promise<string>(resolve => {
+              core.info('Downloading file:')
+              core.info(`\turl: ${url}`)
+              core.info(`\tlocation: ${path.resolve(fileLocation)}`)
+              const download = new DownloaderHelper(url, fileLocation)
+              download.start()
+              download.on('end', ({filePath, fileName}) => {
+                core.info(`File ${fileName} downloaded.`)
+                resolve(filePath)
+              })
+            })
+        )
+      )
     )
-    core.setOutput('files', filePaths.join('\n'))
     core.info('Files successfully downloaded.')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
